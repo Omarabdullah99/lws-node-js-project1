53132fb933fbdb04fea246b3a0bcb6c517053b73
Omarabdullah99 <omarabdullah917303@gmail.com>
2025-01-13 22:30:41 +0600

    index.js inisital file setup and create server

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+/*
+ * Title: Uptime Monitoring Application
+ * Description: A RESTFul API to monitor up or down time of user defined links
+ * Author: Sumit Saha ( Learn with Sumit )
+ * Date: 11/15/2020
+ *
+ */
+
+// dependencies
 const http= require('http')
+
+// app object - module scaffolding
+const app={}
+
+// configuration
+app.config = {
+    port: 3000,
+};
+
+// create server
+app.createServer=()=>{
+    const server= http.createServer(app.hadnlereqres)
+    server.listen(app.config.port,()=>{
+        console.log(`server port is running ${app.config.port}`)
+    })
+}
+
+// handle Request Response
+app.hadnlereqres=(req,res)=>{
+    res.end("hello wrold")
+}
+
+//start the server
+app.createServer()
