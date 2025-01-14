56df23d9245d145fde7d046c699bc802783f5328
Omarabdullah99 <omarabdullah917303@gmail.com>
2025-01-14 15:16:20 +0600

    req related all work

--- index.js
+++ index.js
@@ -7,28 +7,49 @@
  */
 
 // dependencies
-const http= require('http')
+const http = require("http");
+const url = require("url");
+const { StringDecoder } = require("string_decoder");
+const { buffer } = require("stream/consumers");
 
 // app object - module scaffolding
-const app={}
+const app = {};
 
 // configuration
 app.config = {
-    port: 3000,
+  port: 3000,
 };
 
 // create server
-app.createServer=()=>{
-    const server= http.createServer(app.hadnlereqres)
-    server.listen(app.config.port,()=>{
-        console.log(`server port is running ${app.config.port}`)
-    })
-}
+app.createServer = () => {
+  const server = http.createServer(app.hadnlereqres);
+  server.listen(app.config.port, () => {
+    console.log(`server port is running ${app.config.port}`);
+  });
+};
 
 // handle Request Response
-app.hadnlereqres=(req,res)=>{
-    res.end("hello wrold")
-}
+app.hadnlereqres = (req, res) => {
+  //get url and parse it
+  const parseUrl = url.parse(req.url, true);
+  const path = parseUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, "");
+  //get req method(get,post,delete,put,patch)
+  const method = req.method.toLowerCase();
+  //get all req header
+  const headerObject = req.headers;
+  //req body or req payload
+  const decoder = new StringDecoder("utf-8");
+  let realdata = "";
+  req.on("data", (buffer) => {
+    realdata += decoder.write(buffer);
+  });
+  req.on("end", () => {
+    realdata += decoder.end();
+    console.log(realdata);
+  });
+  res.end("hello wrold");
+};
 
 //start the server
-app.createServer()
+app.createServer();
